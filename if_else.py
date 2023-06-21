11692d2908b805944d49663b7ec774e76ad13a26
WolfPlay12 <83851953+WolfPlay12@users.noreply.github.com>
2023-06-20 18:28:18 -0700

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,16 @@
+a = "milk"
+b = "milk"
+
+if(a == b):
+    print("the same")
+else: print("diffrent")
+
+input()
+
+#this checks if the 2 variables are the same as each other
+#if they are the same 1 thing happends 
+#if they are not the same another thing happends
+#if else's will all follow 
+if()
+#your code goes here
+else: #your code goes here
